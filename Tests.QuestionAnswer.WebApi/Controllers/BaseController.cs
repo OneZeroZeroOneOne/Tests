08f090c94a7c58b4c17666b7fee19354200b4739
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2021-02-02 14:18:41 +0200

    up

--- Tests.QuestionAnswer.WebApi/Controllers/BaseController.cs
+++ Tests.QuestionAnswer.WebApi/Controllers/BaseController.cs
@@ -11,13 +11,13 @@ namespace Tests.QuestionAnswer.WebApi.Controllers
 {
     public class BaseController : ControllerBase
     {
-        private bool HasTokenProvided => Request.Headers.ContainsKey("Authorization") ||
+        private bool IsTokenProvided => Request.Headers.ContainsKey("Authorization") ||
                                          Request.Headers.ContainsKey("authorization");
         private StringValues StringToken => Request.Headers.ContainsKey("Authorization")
             ? Request.Headers.GetOrDefault("Authorization")
             : Request.Headers.GetOrDefault("authorization");
 
-        protected JwtSecurityToken? Token => HasTokenProvided ? JwtService.ParseTokenNullSafe(StringToken) : throw new Exception("User don't provided token");
+        protected JwtSecurityToken? Token => IsTokenProvided ? JwtService.ParseTokenNullSafe(StringToken) ?? throw new Exception("Can't parse token") : throw new Exception("User don't provided token");
 
         protected int UserId => int.Parse(Token.Claims.First(x => x.Type == ClaimsIdentity.DefaultNameClaimType).Value);
     }
